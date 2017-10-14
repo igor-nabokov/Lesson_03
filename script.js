7dac910141260b0f95d0dc42aa402acd75ae201a
igor-nabokov <baldur.hermod@gmail.com>
2017-10-14 19:47:26 +0300

    added js code

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function writeZero() {
-	console.log('u');
+function writeZero(div) {
+	if (div.innerHTML == "") {
+		div.innerHTML = "o";
+		writeEx();
+	}
 }
 
-alert('kigiugiugiu');
+function writeEx() {
+	var cells = document.querySelectorAll(".cell");
+	var emptyCells = [];
+	for (var i = 0; i < cells.length; i++) {
+		if (cells[i].innerHTML == "") {
+			emptyCells.push(cells[i]);
+		}
+	}
+	var random = Math.floor(Math.random() * emptyCells.length);
+	emptyCells[random].innerHTML = "x";
+}
+
+window.onload = function () {
+	writeEx();
+};
+
